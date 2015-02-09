f197b97cb6fa80c57cdbaf1abfbcaff06339c5f8
simransachar <ssachar@umd.edu>
2015-02-09 01:02:52 -0500

    feb 8th

--- access_api.py
+++ access_api.py
@@ -6,12 +6,12 @@ from calculate_score import calcPersonalXPScores, calcReadability
 article_text = "Sheila Heen, an expert in negotiation and difficult"
 
 #url = "http://ec2-54-173-77-171.compute-1.amazonaws.com/update_demo_comment"
-#url = "http://127.0.0.1:5000/commentIQ/v1/updateComment"
+url = "http://127.0.0.1:5000/commentIQ/v1/updateComment"
 #url = "http://127.0.0.1:5000/commentIQ/v1/updateArticle"
 #url = "http://127.0.0.1:5000/commentIQ/v1/addArticle"
 #url = "http://127.0.0.1:5000/commentIQ/v1/addComment"
 #url = "http://ec2-54-173-77-171.compute-1.amazonaws.com/commentIQ/v1/addComment"
-url = "http://ec2-54-173-77-171.compute-1.amazonaws.com/commentIQ/v1/updateComment"
+#url = "http://ec2-54-173-77-171.compute-1.amazonaws.com/commentIQ/v1/updateComment"
 articleID = 78
 commentID = 172
 comment_text = "Oh how true - you nailed it again! In my case, viewing the entire season of Downton Abbey - accompanied by a healthy swoon - was the best anti-dote possible to having accidentally exposed myself to the first (and for me the last) season of Breaking Bad. And I admit to an ongoing crush on Mr. Bates"
@@ -32,8 +32,8 @@ print response.json()
 #url = "http://ec2-54-173-77-171.compute-1.amazonaws.com/get_scores/'"+ str(commentID) +"'"
 #url = "http://ec2-54-173-77-171.compute-1.amazonaws.com/get_ArticleRelevance/'"+ str(commentID) +"'"
 #url = 'http://127.0.0.1:5000/delete_demo_comment/"' + `commentID` + '"'
-#url = "http://127.0.0.1:5000/commentIQ/v1/getScores/'"+ str(commentID) +"'"
-url = "http://ec2-54-173-77-171.compute-1.amazonaws.com/commentIQ/v1/getScores/'"+ str(commentID) +"'"
+url = "http://127.0.0.1:5000/commentIQ/v1/getScores/'"+ str(commentID) +"'"
+#url = "http://ec2-54-173-77-171.compute-1.amazonaws.com/commentIQ/v1/getScores/'"+ str(commentID) +"'"
 #url = "http://127.0.0.1:5000/commentIQ/v1/getConversationalRelevance/'"+ str(commentID) +"'"
 #url = "http://127.0.0.1:5000/commentIQ/v1/getArticleRelevance/'"+ str(commentID) +"'"
 #url = "http://127.0.0.1:5000/commentIQ/v1/getPersonalXP/'"+ str(commentID) +"'"
